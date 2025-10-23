9f2270e3852ea42102ba58c3c2d0fc3e6fea7ae9
terry zou <terryzou92324@gmail.com>
2025-10-23 15:11:07 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pic1.imgdb.cn',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
