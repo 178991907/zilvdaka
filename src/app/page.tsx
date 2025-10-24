abd8408c110a553d4dd706535713c688d8e3ce82
terry zou <terryzou92324@gmail.com>
2025-10-24 03:34:30 +0000

    2025年10月24日 星期五 11:33:54 移至宠物下面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,15 +48,12 @@ export default function LandingPage() {
         </nav>
       </header>
 
-      <div className="flex justify-center py-4">
-        <DigitalClock />
-      </div>
-
       <main className="flex-1 flex flex-col items-center justify-start px-4 md:px-8">
         <div className="flex flex-col md:flex-row items-end justify-center gap-8 w-full max-w-4xl">
           {/* Left Column: Pet Viewer */}
-          <div className="w-full md:w-1/2 flex justify-center">
+          <div className="w-full md:w-1/2 flex flex-col items-center justify-center gap-4">
             {user && <PetViewer progress={petProgress} className="w-80 h-80" />}
+            <DigitalClock />
           </div>
 
           {/* Right Column: Stats */}
