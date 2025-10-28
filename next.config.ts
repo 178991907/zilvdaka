31f51f059a5b51b379482cedaf1f93087448761d
terry zou <terryzou92324@gmail.com>
2025-10-28 22:44:01 +0000

    执行

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    forceSwcTransforms: true,
-  },
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
